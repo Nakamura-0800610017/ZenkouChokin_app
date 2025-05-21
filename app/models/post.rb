8fdf1f84f64075ce1f81b40300e9fa4a934eb462
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-05-21 18:16:40 +0900

    update:postsモデルとusersモデルのアソシエーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     validates :body, presence: true, length: { maximum: 140 }
+    validates :point, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: -10, less_than_or_equal_to: 10 }
+
+    belongs_to :user
 end
