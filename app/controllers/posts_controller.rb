6086a2f9d5ce5e4626dccaf3635885f272136f36
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-05-23 17:09:41 +0900

    update:Lintチェックの指摘事項を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  skip_before_action :require_login, only: [:index]
+  skip_before_action :require_login, only: [ :index ]
   def index
     @posts = Post.all.order(created_at: :desc)
   end
