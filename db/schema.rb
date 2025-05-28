014d1e0742c8f7538f54be3b009aae8c00899aad
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-05-28 18:16:15 +0900

    add:postsテーブルにpost_typeカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_21_091120) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_28_090623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_21_091120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "post_type", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
