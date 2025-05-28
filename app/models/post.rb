e3719f3c5d33f603ef7fd24705b4d1408ea452d8
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-05-28 18:26:30 +0900

    update:post_typeのenumを定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     validates :body, presence: true, length: { maximum: 140 }
     validates :point, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: -10, less_than_or_equal_to: 10 }
+    enum post_type: { zenkou: 0, akugyou: 1 }
 
     belongs_to :user
 end
