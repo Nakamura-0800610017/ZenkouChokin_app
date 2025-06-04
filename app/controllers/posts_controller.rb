7619cd463f9733ec7052c6ea165201f7c162feb6
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-06-04 12:19:08 +0900

    update:投稿用アクションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,39 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
   end
+
+def new_zenkou
+  @post = Post.new(post_type: :zenkou)
+  render :new
+end
+
+def new_akugyou
+  @post = Post.new(post_type: :akugyou)
+  render :new
+end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, success: "投稿しました"
+    else
+      flash.now[:danger] = "投稿に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def point_select
+    @post = Post.new(post_params)
+    render turbo_stream: turbo_stream.replace(
+    "point_select",
+    partial: "posts/point_select",
+    locals: { post: @post }
+  )
+end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post_type, :body, :point)
+  end
 end
