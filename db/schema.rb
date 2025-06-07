fa370f89511b2e7fc3c05b3fb5a24275c1816602
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-06-07 15:47:23 +0900

    add:user_pointsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_28_090623) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_07_062530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_28_090623) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "user_points", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "total_points", default: 0, null: false
+    t.integer "user_rank", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["total_points"], name: "index_user_points_on_total_points"
+    t.index ["user_id"], name: "index_user_points_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -35,4 +45,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_28_090623) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "user_points", "users"
 end
