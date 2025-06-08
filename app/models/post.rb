1276711a4e57f97eb3ae09824396229c7049dd34
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-06-08 19:05:41 +0900

    update:total_point計算メソッドを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,18 @@
 class Post < ApplicationRecord
-    validates :body, presence: true, length: { maximum: 140 }
-    validates :point, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: -10, less_than_or_equal_to: 10 }
-    enum post_type: { zenkou: 0, akugyou: 1 }
+  validates :body, presence: true, length: { maximum: 140 }
+  validates :point, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: -10, less_than_or_equal_to: 10 }
+  enum post_type: { zenkou: 0, akugyou: 1 }
 
-    belongs_to :user
+  belongs_to :user
+
+  after_create :update_total_points
+  after_destroy :update_total_points
+
+  private
+
+  def update_total_points
+    user_point = user.user_point || user.create_user_point # 検証用アカウントを削除したら消しても良い
+    user_point.total_points = user.posts.sum(:point)
+    user_point.update_rank!
+  end
 end
