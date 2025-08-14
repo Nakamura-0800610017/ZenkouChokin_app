05b9f9726f176737c36276f7131ae1b61354107b
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-08-14 21:28:20 +0900

    update:ストロングパラメーターの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :terms_of_service)
   end
 end
