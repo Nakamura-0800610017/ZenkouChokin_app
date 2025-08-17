390adc7e66292514dc3e3f3d778fa3ab92871462
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-08-17 16:21:49 +0900

    fix:js読み込みエラーの修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,10 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application.js"
 
-import HelloController from "./hello_controller"
+import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
 
-import TermController from "./term_controller"
+import TermsController from "./terms_controller.js"
 application.register("terms", TermsController)
