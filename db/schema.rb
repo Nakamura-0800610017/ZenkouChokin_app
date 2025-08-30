db6184a39fd6ba9181d8286a131a8d88f52eb82a
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-08-30 15:13:48 +0900

    add:RoleカラムをUsersテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_28_062446) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_30_060818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_28_062446) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.integer "mode", default: 0, null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["mode"], name: "index_users_on_mode"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
