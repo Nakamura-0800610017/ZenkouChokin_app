a17f3e20a306c16d6112d71f83f4ef1e54a71605
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-09-01 14:12:50 +0900

    update:edit,update,destroyメソッド追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,4 +2,30 @@ class Admin::PostsController < Admin::BaseController
   def index
     @posts = Post.includes(:user).order(created_at: :desc).page(params[:page])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to admin_root_path, success: t("default.flash_message.updated", item: Post.model_name.human)
+    else
+      flash.now[:danger] = t("default.flash_message.not_updated", item: Post.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+    redirect_to admin_root_path, success: t("default.flash_message.deleted", item: Post.model_name.human), status: :see_other
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post_type, :body, :point)
+  end
 end
