9c86b1f8d70f714d6afc356f96fb6a2cbdd7f420
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-09-03 18:43:36 +0900

    update:authenticationsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_01_103231) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_03_092145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+  end
 
   create_table "bookmarks", force: :cascade do |t|
     t.bigint "user_id"
