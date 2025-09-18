1a1300784ec383fa0a9bf46a6d696dffba5f02a7
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-09-19 00:00:59 +0900

    add:post_spec作成

---
+++ spec/factories/posts.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :post do
+    association :user
+    body { Faker::String.random(length: 50) }
+    point { Faker::Number.between(from: 1, to: 10) }
+    post_type { :zenkou }
+
+    trait :akugyou do
+      point { Faker::Number.between(from: -10, to: -1) }
+      post_type { :akugyou }
+    end
+  end
+end
