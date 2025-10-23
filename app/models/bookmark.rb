4812cf2cf5a0825b8380a6128d2c52c3dde262a4
NakamuraKanako-0800610017 <akitakomachi@me.com>
2025-10-23 18:16:08 +0900

    update:未ログインユーザーの作成に対応したバリデーションの追加

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,11 +1,18 @@
 class Bookmark < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :post
 
-  validates :user_id, uniqueness: { scope: :post_id }
+  validates :post_id, presence: true
+  validate :user_or_session_present
 
   after_create :update_bookmark_bonus
   after_destroy :update_bookmark_bonus
+
+  def user_or_session_present
+    if user_id.blank? && session_id.blank?
+      errors.add(:base, "『いいね』に失敗しました")
+    end
+  end
 
   def update_bookmark_bonus
     post.user.user_point.update_total_points!
